dfcffb4eea79b1c9ab47712186a5742e8d95e9ec
Michael Haslgrübler <work-michael@haslgruebler.eu>
2014-05-21 01:16:01 +0200

    initial debian packaging release

---
+++ debian/extra/bin/setenv.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+
+CATALINA_HOME=/usr/share/tomcat7
+CLASSPATH=$CATALINA_HOME/lib/
---
+++ debian/extra/bin/startup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export CATALINA_BASE="/var/lib/hpassword"
+/usr/share/tomcat7/bin/startup.sh
+echo "Tomcat started"
